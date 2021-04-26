7f33eb34c5a22ffb487a894591276b359d275fcf
aylenalderete <aylenalderete@hotmail.com>
2021-04-26 17:46:08 -0300

    'Imagenes para reuniones'

--- src/Components/Home/Reuniones.jsx
+++ src/Components/Home/Reuniones.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Imagen from '../../Assets/Carrousel/1.jpg'
+import ReunionJueves from '../../Assets/Reuniones/ReunionJueves.jpeg'
+import ReunionDomingo from '../../Assets/Reuniones/ReunionDomingo.jpeg'
 import Styles from './Reuniones.module.css'
 
 function Reuniones() {
     return (
         <div className={Styles.cardsContainer}>
             <div className={Styles.card}>
-                <img className={Styles.img} src={Imagen}></img>
+                <img className={Styles.img} src={ReunionJueves}></img>
                 <spam className={Styles.cardTitle}>Jueves</spam>
                 <spam className={Styles.description}>Reunion general 20hs</spam>
                 <button className={Styles.btn}>Ver culto online</button>
@@ -18,7 +20,7 @@ function Reuniones() {
                 <button className={Styles.btn}>Ver culto online</button>
             </div>
             <div className={Styles.card}>
-                <img className={Styles.img} src={Imagen}></img>
+                <img className={Styles.img} src={ReunionDomingo}></img>
                 <spam className={Styles.cardTitle}>Domingo</spam>
                 <spam className={Styles.description}>Reunion general 19hs</spam>
                 <button className={Styles.btn}>Ver culto online</button>
